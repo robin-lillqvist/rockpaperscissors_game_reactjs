e70984c451d8fb7f5661270357927068441eb0bb
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-04 13:27:34 +0100

    added cypress tests to check if page loads successfully and you're able to click the button

--- cypress/integration/userCanSeeFrontpage.feature.js
+++ cypress/integration/userCanSeeFrontpage.feature.js
@@ -4,19 +4,14 @@ describe('User open page to see the three selections', () => {
   })
 
   it('can see the Title', () => {
-    cy.get('#selections').should('contain', 'Rock Paper Scissors')
-      })
-
-  it('can see the background image', () => {
-    cy.get('body').should('have', 'Rock Paper Scissors')
-    cy.get('body').should('have.attr', 'background-image', '/users')
+    cy.get('h1').should('contain', 'Rock, Paper, Scissors!')
       })
 
   it('can see three selections', () => {
-    cy.get('#selections').within(() => {
+    cy.get('.button-container').within(() => {
         cy.get('#rock')
         cy.get('#paper')
-        cy.get('#scissor')
+        cy.get('#scissors')
     })
   })
 });
