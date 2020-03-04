1b251efc76aab2789afd5f9ab698ef14cea4d7ef
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-03-04 13:33:42 +0100

    added fonts in index.html and updated App.jsx to include components written to show page also constructed playRound method in App.jsx that take user input and simulate computer action

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,73 @@
 import React, { Component } from 'react'
-import Selection from './components/showSelections'
 import Header from './components/Header'
+import ShowResults from './components/ShowResult'
+import ShowCombo from './components/ShowCombo'
+import { doBattle } from './helpers/battleHelper'
 import './App.css'
-import Opponent from './components/Opponent';
+
+const weaponChoices = ['Rock', 'Paper', 'Scissors']
 
 class App extends Component {
-  
   state = {
-    backgroundImage: "url('./img/rock.png')"
-  };
+    opponentChoice: '',
+    playerChoice: '',
+    result: '',
+    combo: 0
+  }
+
+  playRound (event) {
+    let player = event.target.value
+    let opponent = weaponChoices[Math.floor(Math.random() * weaponChoices.length)]
+    let results = doBattle(player, opponent)
+    this.setState({
+      playerChoice: player,
+      opponentChoice: opponent,
+      result: results
+    })
+    if (results === 'wisely') {
+      let new_combo = this.state.combo
+      new_combo++
+      this.setState({ combo: new_combo })
+    } else { //if(results === 'poorly')
+      this.setState({ combo: 0 })
+    }
+  }
 
   render () {
     return (
+      <>
+        <Header />
 
-        <div>
-          <Header />
-          <Opponent />
-          <Selection />
+        <ShowCombo 
+        combo={this.state.combo}
+        />
+        
+        <ShowResults
+          playerChoice={this.state.playerChoice}
+          opponentChoice={this.state.opponentChoice}
+          result={this.state.result}
+        />
+
+        <div className='ui very padded text container'>
+          <div class='button-container'>
+            <button
+              onClick={event => this.playRound(event)}
+              id='rock'
+              value='Rock'
+            ></button>
+            <button
+              onClick={event => this.playRound(event)}
+              id='paper'
+              value='Paper'
+            ></button>
+            <button
+              onClick={event => this.playRound(event)}
+              id='scissors'
+              value='Scissors'
+            ></button>
+          </div>
         </div>
+      </>
     )
   }
 }
